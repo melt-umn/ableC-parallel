b4a9673c31788ca5e24e8b2579d26ca06d7b3b08
Aaron Councilman <counc009@umn.edu>
2021-05-25 14:48:55 -0500

    Cleanup workstlr example artifact

--- examples/workstlr/artifact/Artifact.sv
+++ examples/workstlr/artifact/Artifact.sv
@@ -12,7 +12,6 @@ parser extendedParser :: cst:Root {
   edu:umn:cs:melt:exts:ableC:cilk;
   edu:umn:cs:melt:exts:ableC:parallel;
   edu:umn:cs:melt:exts:ableC:parallel:impl:workstlr;
-  edu:umn:cs:melt:exts:ableC:parallel:impl:blocking;
   edu:umn:cs:melt:exts:ableC:parallel:impl:posix;
 }
 
